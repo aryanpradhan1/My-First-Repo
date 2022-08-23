48a9b0309e2551a61316946a1f995dda34475246
aryanpradhan1 <109923956+aryanpradhan1@users.noreply.github.com>
2022-08-23 14:19:37 -0500

    BlueJ and Notes

---
+++ My-First-BlueJ-Project/ch1Notes.java
@@ -0,0 +1,43 @@
+
+/**
+ * Aryan Pradhan
+ * 8/23/22
+ * Chapter 1 Notes
+ * Notes from class
+ * Who helped me: Mr. Hayes
+ * 
+ */
+import java.util.Scanner;//class is last arg
+import java.util.Random;
+
+
+
+public class ch1Notes
+{
+  
+    /**
+     * this is code notes for ch 1
+     */
+    public static void main (String[] args)
+    {
+      Scanner keyboard = new Scanner(System.in);
+      
+      // basic data types
+      int x = 9,y,z;
+      boolean choice = false;
+      char letter = 'b';
+      float number2 = 9;
+      double number1 = 8.9, number3;
+      short numb = 6;
+      long numberrrr = 9999;
+      
+      
+      //These are Objects
+      //Class  id       memory  Constructor
+      String   word =    new    String("Kevin Hayes");
+      Integer  number =  new    Integer(47);
+      Double   anumber = new    Double(45.8);
+      
+      System.out.print(word);
+    }
+}
